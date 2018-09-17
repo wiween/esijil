5f4382d5ba3749deeda15d90071545595bee5dae
mdridzuan80 <md.ridzuan80@gmail.com>
2018-09-18 02:53:40 +0800

    trusted proxy

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,7 +34,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->redirectTo = url('dashboard');
         $this->middleware('guest')->except('logout');
     }
 }
