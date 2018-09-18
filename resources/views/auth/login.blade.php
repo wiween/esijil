13d62a512ee94ef56fac11f0a9f3e292f7a05ecb
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2018-09-19 05:24:50 +0800

    fix image url

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"> {{ __('Login') }}<img src="{{ url('images/jata.png') }}"></div>
+                <div class="card-header"> {{ __('Login') }}<img src="{{ asset('images/jata.jpg') }}"></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
