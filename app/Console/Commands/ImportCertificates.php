129c6e090a9f61f8d044d837981f83f3721eed50
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2018-09-18 22:50:23 +0800

    Split resource cert in repository

--- app/Console/Commands/ImportCertificates.php
+++ app/Console/Commands/ImportCertificates.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\Certificate;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
+use App\Repositories\CertificateSource;
 
 class ImportCertificates extends Command
 {
@@ -30,11 +30,13 @@ class ImportCertificates extends Command
      */
 
     private $client = null;
+    private $certificateSource = null;
 
-    public function __construct(Client $guzzleClient)
+    public function __construct(Client $guzzleClient, CertificateSource $certificateSource)
     {
         parent::__construct();
         $this->client = $guzzleClient;
+        $this->certificateSource = $certificateSource;
     }
 
     /**
@@ -48,31 +50,7 @@ class ImportCertificates extends Command
         /* $response = $this->client->request('GET', env('MOSQ_IMPORT_CERTIFICATES_URL'));
         $data = json_decode($response->getBody()); */
 
-        $data = DB::select("select c.nama_pelatih as name, c.ic_or_passport as ic_number,
-            d.nama_program as programme_name,
-            d.kod_program as programme_code, 'pb' as type,
-            substring_index(substring_index(d.kod_program,':', 1),'-',-1) as level,
-            e.nama_pusat as pb_name, g.id as state_id,
-            if(f.to_visit_date_tamat = '0000-00-00', null, date_format(to_visit_date_tamat,'%m%Y')) as date_ppl,
-            a.keputusan_ppl as result_ppl, b.no_batch as batch_id, IFNULL(c.no_rumah, e.alamat_sykt) as address
-            from mosq.penilaian_bukan_kredit as a
-            join mosq.daftar_batch as b on a.batch_id = b.id
-            join mosq.profil_pelatih as c on a.pelatih_id = c.id
-            join mosq.program as d on b.program_id = d.id
-            join mosq.pb as e on b.pusat_id = e.id
-            left join mosq.urus_ppl as f on a.urus_ppl_id = f.id
-            join mosq.negeri as g on g.kod_negeri = e.kod_negeri
-            where 1 = 1
-            and (a.keputusan_ppl = 1
-            or a.keputusan_jpp = 1)
-            union select a.nama as name, a.no_ic as ic_number, b.nama_program as programme_name, a.kod_program as programme_code, 
-            a.jenis_tauliah as type, substring_index(substring_index(a.kod_program,':', 1),'-',-1) as level,
-            c.nama_pusat as pb_name, ifnull(d.id,1) as state_id, a.tarikh_ppl as date_ppl, null as result_ppl,
-            a.no_batch as batch_id, c.alamat_sykt as address
-            from mosq.skm as a
-            left join mosq.program as b on b.kod_program = a.kod_program
-            left join mosq.pb as c on a.kod_pusat = c.kod_pusat
-            left join mosq.negeri as d on d.kod_negeri = c.kod_negeri");
+        $data = $this->certificateSource->certSource();
         
         $bar = $this->output->createProgressBar(count($data));
 
