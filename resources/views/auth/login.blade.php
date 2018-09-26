7ea36eeda9bcc3c71f7b8de5de7f2d7c6df4470f
Md Ridzuan bin Mohammad Latiah <wanlatiah@gmail.com>
2018-09-26 10:19:30 +0800

    Fix #4 Enhance Login Page View

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.app_login')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+             <div class="row justify-content-center">
+                <img src="{{ asset('images/jata.png') }}" alt="Jata Malaysia">
+            </div>
+            <div class="row justify-content-center">
+                <h1>Sistem eSijil</h1>
+            </div>
+            <div class="row justify-content-center">
+                <h2>Jabatan Pembangunan Kemahiran</h2>
+            </div>
+             <div class="row justify-content-center">
+                <h2>Kementerian Sumber Manusia</h2>
+            </div>
             <div class="card">
-
-                <div class="card-header"><img src="{{ asset('images/jatasmall.jpg') }}"> {{ __('Login') }}</div>
-
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
