6c9bf02789f2703e7f6b5666b25a530930982b39
Md Ridzuan bin Mohammad Latiah <wanlatiah@gmail.com>
2018-10-05 07:48:50 +0800

    add db namespace

--- database/migrations/2018_09_27_015028_create_sysvars_table.php
+++ database/migrations/2018_09_27_015028_create_sysvars_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
