f84cde7d3ca386b68e98b83943ae4be094a17615
Md Ridzuan bin Mohammad Latiah <wanlatiah@gmail.com>
2018-12-07 15:49:08 +0800

    Add error exception when import data mosq

--- app/Console/Commands/ImportCertificates.php
+++ app/Console/Commands/ImportCertificates.php
@@ -50,47 +50,46 @@ class ImportCertificates extends Command
         $data = json_decode($response->getBody()); */
 
         $data = $this->certificateSource->certSource();
-        
+
         $bar = $this->output->createProgressBar(count($data));
 
-        foreach($data as $row)
-        {   
-            if($this->getOutput()->isVerbose())
-                $this->info("\nImport ". $row->name);
+        foreach ($data as $row) {
+            if ($this->getOutput()->isVerbose())
+                $this->info("\nImport " . $row->name);
 
-            Certificate::updateOrCreate(
-                [
-                    'ic_number' => trim($row->ic_number),
-                    'batch_id' => trim($row->batch_id),
-                    'programme_code' => trim($row->programme_code),
-                ],
-                [
-                    'name' => trim($row->name),
-                    'ic_number' => trim($row->ic_number),
-                    'programme_name' => trim ($row->programme_name),
-                    'programme_code' => trim ($row->programme_code),
-                    'type' => trim ($row->type),
-                    'level' => strtoupper($this->constructLevel($row, 'tahap')),
-                    'kod_pusat' => trim($row->kod_pusat),
-                    'pb_name' => trim ($row->pb_name),
-                    'state_id' => trim ($row->state_id),
-                    'date_ppl' => trim ($row->date_ppl),
-                    'result_ppl' => trim ($row->result_ppl),
-                    'batch_id' => trim ($row->batch_id),
-                    'address' => trim ($row->address),
-                    'tarikh_ppl' => ($row->tarikh_ppl) ? trim ($row->tarikh_ppl) : null,
-                    'nama_syarikat' => trim ($row->nama_syarikat),
-                    'negeri_syarikat' => trim ($row->negeri_syarikat),
-                    'ndt_sah_mula' => ($row->ndt_sah_mula) ? trim ($row->ndt_sah_mula) : null,
-                    'ndt_sah_tamat' => ($row->ndt_sah_tamat) ? trim ($row->ndt_sah_tamat) : null,
-                    'tarikh_ndt_terdahulu' => ($row->tarikh_ndt_terdahulu)?trim ($row->tarikh_ndt_terdahulu) : null,
-                    'tarikh_mesy_ndt' => ($row->tarikh_mesy_ndt && $row->tarikh_mesy_ndt !== '0000-00-00') ? trim ($row->tarikh_mesy_ndt) : null,
-                    'nama_program_terdahulu' => trim ($row->nama_program_terdahulu),
-                    'no_sijil_dahulu' => trim ($row->no_sijil_dahulu),
-                    'tarikh_sijil_baru_mula' => ($row->tarikh_sijil_baru_mula) ? trim ($row->tarikh_sijil_baru_mula) : null,
-                    'jenis_sijil' => trim ($row->jenis_sijil),
-                ]
-            );
+            try {
+                Certificate::updateOrCreate(
+                    ['ic_number' => trim($row->ic_number), 'batch_id' => trim($row->batch_id), 'programme_code' => trim($row->programme_code)],
+                    [
+                        'name' => trim($row->name) ?? null,
+                        'ic_number' => trim($row->ic_number) ?? null,
+                        'programme_name' => trim($row->programme_name) ?? null,
+                        'programme_code' => trim($row->programme_code) ?? null,
+                        'type' => trim($row->type) ?? null,
+                        'level' => strtoupper($this->constructLevel($row, 'tahap')) ?? null,
+                        'kod_pusat' => trim($row->kod_pusat) ?? null,
+                        'pb_name' => trim($row->pb_name) ?? null,
+                        'state_id' => trim($row->state_id) ?? null,
+                        'date_ppl' => trim($row->date_ppl) ?? null,
+                        'result_ppl' => trim($row->result_ppl) ?? null,
+                        'batch_id' => trim($row->batch_id) ?? null,
+                        'address' => trim($row->address) ?? null,
+                        'tarikh_ppl' => trim($row->tarikh_ppl) ?? null,
+                        'nama_syarikat' => trim($row->nama_syarikat) ?? null,
+                        'negeri_syarikat' => trim($row->negeri_syarikat),
+                        'ndt_sah_mula' => trim($row->ndt_sah_mula) ?? null,
+                        'ndt_sah_tamat' => trim($row->ndt_sah_tamat) ?? null,
+                        'tarikh_ndt_terdahulu' => trim($row->tarikh_ndt_terdahulu) ?? null,
+                        'tarikh_mesy_ndt' => ($row->tarikh_mesy_ndt && $row->tarikh_mesy_ndt !== '0000-00-00') ? trim($row->tarikh_mesy_ndt) : null,
+                        'nama_program_terdahulu' => trim($row->nama_program_terdahulu) ?? null,
+                        'no_sijil_dahulu' => trim($row->no_sijil_dahulu) ?? null,
+                        'tarikh_sijil_baru_mula' => trim($row->tarikh_sijil_baru_mula) ?? null,
+                        'jenis_sijil' => trim($row->jenis_sijil) ?? null,
+                    ]
+                );
+            } catch (\Exception $e) {
+                echo "WARNING!: " . $e->getMessage();
+            }
 
             $bar->advance();
         }
@@ -102,8 +101,7 @@ class ImportCertificates extends Command
 
     private function constructLevel($record, $pre = '')
     {
-        if($record->type == 'pb')
-        {
+        if ($record->type == 'pb') {
             if (strpos($record->programme_code, ':') || strpos($record->programme_code, ';')) {
                 if (strpos($record->programme_code, ':'))
                     return $this->hasYearDeli($record->programme_code, $pre, ':');
@@ -115,9 +113,7 @@ class ImportCertificates extends Command
 
                 return null;
             }
-        }
-        else
-        {
+        } else {
             return trim($pre) . " " . $this->certificateSource->numToWord($record->level);
         }
     }
