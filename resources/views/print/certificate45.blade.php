86de891a8bff107a9e199c3a0465bcd9e4227ddf
Md Ridzuan bin Mohammad Latiah <wanlatiah@gmail.com>
2019-01-04 11:31:32 +0800

    adjust laporan print sijil

--- resources/views/print/certificate45.blade.php
+++ resources/views/print/certificate45.blade.php
@@ -35,49 +35,68 @@ if ( isset($pdf) ) {
 </script>
     <main>
    @foreach($certificates as $certificate)
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <div class="center">
-            <h2 align="center">{{ strtoupper($certificate->programme_name) }}</h2>
-        </div>
-        <br/>
-        <br/>
-        <br/>
-        <div class="center">
-            <h2 align="center">{{ strtoupper($certificate->name) }}</h2>
-            <h2 align="center">{{  $certificate->ic_number }}</h2>
-        </div>
-        <br/>
-            <br/>
-            <br/>
-            <br/>
-            <div class="center">
-            <h3 align="center">PENILAIAN TELAH DILAKSANAKAN DI PUSAT BERTAULIAH <BR></h3>
-            <h3 align="center">{{  strtoupper($certificate->pb_name) }}</h3>
-        </div>
-        <br/>
-        <br/>
-        <br/>
-            <br/>
+        <table width="100%">
+            <tr><td>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <div class="center">
+                    <h2 align="center">{{ strtoupper($certificate->programme_name) }}</h2>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <div class="center">
+                    <h2 align="center">{{ strtoupper($certificate->name) }}</h2>
+                    <h2 align="center">{{  $certificate->ic_number }}</h2>
+                </div>
+                <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div class="center">
+                    <h3 align="center">PENILAIAN TELAH DILAKSANAKAN DI PUSAT BERTAULIAH <BR></h3>
+                    <h3 align="center">{{  strtoupper($certificate->pb_name) }}</h3>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                    <br/>
 
-           <div class="footer">
-            <h5>{{  strtoupper($certificate->batch_id) }}<br>
-            {{  strtoupper($certificate->date_print->format('d/m/Y')) }}<br>
-           {{  strtoupper($certificate->printed_remark) }}</h5>
+            </td></tr>
+        </table>
+
+        <div class="information" style="position: absolute; bottom: 0;">
+            <table width="100%">
+                <tr>
+                    <td align="left" style="width: 50%; height: 150px; vertical-align: top; padding-left: 100px; ">
+                    <h5>
+                        {{ strtoupper($certificate->batch_id) }}
+                        <br>
+                        {{ strtoupper($certificate->date_print->format('d/m/Y')) }}
+                        <br>
+                        {{ strtoupper($certificate->printed_remark) }}
+                    </h5>
+                    </td>
+                </tr>
+            </table>
         </div>
+        
+        @if (! $loop->last)
+            <div class="page-break"></div>
+        @endif
 
     @endforeach
 </main>
